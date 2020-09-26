70a496b90727e072500edf41d88153e9c2718600
Brandon Pina <brandonpi265@gmail.com>
2020-09-25 23:06:26 -0500

    Select files, decompress with ffmpeg and score with vmaf

--- vmaf-gui/Form1.Designer.cs
+++ vmaf-gui/Form1.Designer.cs
@@ -28,13 +28,131 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnSource = new System.Windows.Forms.Button();
+            this.btnCompressed = new System.Windows.Forms.Button();
+            this.originalFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.compressedFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.lblSource = new System.Windows.Forms.Label();
+            this.lblCompressed = new System.Windows.Forms.Label();
+            this.vmafLogo = new System.Windows.Forms.PictureBox();
+            this.grpFiles = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.vmafLogo)).BeginInit();
+            this.grpFiles.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSource
+            // 
+            this.btnSource.Location = new System.Drawing.Point(6, 19);
+            this.btnSource.Name = "btnSource";
+            this.btnSource.Size = new System.Drawing.Size(75, 23);
+            this.btnSource.TabIndex = 0;
+            this.btnSource.Text = "Source";
+            this.btnSource.UseVisualStyleBackColor = true;
+            this.btnSource.Click += new System.EventHandler(this.btnSource_Click);
+            // 
+            // btnCompressed
+            // 
+            this.btnCompressed.Location = new System.Drawing.Point(6, 61);
+            this.btnCompressed.Name = "btnCompressed";
+            this.btnCompressed.Size = new System.Drawing.Size(75, 23);
+            this.btnCompressed.TabIndex = 1;
+            this.btnCompressed.Text = "Compressed";
+            this.btnCompressed.UseVisualStyleBackColor = true;
+            this.btnCompressed.Click += new System.EventHandler(this.btnCompressed_Click);
+            // 
+            // originalFileDialog
+            // 
+            this.originalFileDialog.InitialDirectory = ".";
+            this.originalFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.originalFileDialog_FileOk);
+            // 
+            // compressedFileDialog
+            // 
+            this.compressedFileDialog.InitialDirectory = ".";
+            this.compressedFileDialog.FileOk += new System.ComponentModel.CancelEventHandler(this.compressedFileDialog_FileOk);
+            // 
+            // lblSource
+            // 
+            this.lblSource.AutoSize = true;
+            this.lblSource.Location = new System.Drawing.Point(6, 45);
+            this.lblSource.Name = "lblSource";
+            this.lblSource.Size = new System.Drawing.Size(99, 13);
+            this.lblSource.TabIndex = 2;
+            this.lblSource.Text = "No source selected";
+//            this.lblSource.Click += new System.EventHandler(this.lblSource_Click);
+            // 
+            // lblCompressed
+            // 
+            this.lblCompressed.AutoSize = true;
+            this.lblCompressed.Location = new System.Drawing.Point(6, 87);
+            this.lblCompressed.Name = "lblCompressed";
+            this.lblCompressed.Size = new System.Drawing.Size(80, 13);
+            this.lblCompressed.TabIndex = 3;
+            this.lblCompressed.Text = "No file selected";
+//            this.lblCompressed.Click += new System.EventHandler(this.lblCompressed_Click);
+            // 
+            // vmafLogo
+            // 
+            this.vmafLogo.BackgroundImage = global::vmaf_gui.Properties.Resources.vmaf_logo;
+            this.vmafLogo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.vmafLogo.Location = new System.Drawing.Point(34, 12);
+            this.vmafLogo.Name = "vmafLogo";
+            this.vmafLogo.Size = new System.Drawing.Size(160, 131);
+            this.vmafLogo.TabIndex = 4;
+            this.vmafLogo.TabStop = false;
+            // 
+            // grpFiles
+            // 
+            this.grpFiles.Controls.Add(this.btnSource);
+            this.grpFiles.Controls.Add(this.btnCompressed);
+            this.grpFiles.Controls.Add(this.lblCompressed);
+            this.grpFiles.Controls.Add(this.lblSource);
+            this.grpFiles.Location = new System.Drawing.Point(12, 149);
+            this.grpFiles.Name = "grpFiles";
+            this.grpFiles.Size = new System.Drawing.Size(191, 112);
+            this.grpFiles.TabIndex = 5;
+            this.grpFiles.TabStop = false;
+            this.grpFiles.Text = "Select video files";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 267);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(191, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Get VMAF score";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(511, 511);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.grpFiles);
+            this.Controls.Add(this.vmafLogo);
+            this.Name = "Form1";
+            this.Text = "VMAF-GUI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.vmafLogo)).EndInit();
+            this.grpFiles.ResumeLayout(false);
+            this.grpFiles.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSource;
+        private System.Windows.Forms.Button btnCompressed;
+        private System.Windows.Forms.OpenFileDialog originalFileDialog;
+        private System.Windows.Forms.OpenFileDialog compressedFileDialog;
+        private System.Windows.Forms.Label lblSource;
+        private System.Windows.Forms.Label lblCompressed;
+        private System.Windows.Forms.PictureBox vmafLogo;
+        private System.Windows.Forms.GroupBox grpFiles;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- vmaf-gui/Form1.cs
+++ vmaf-gui/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
 
 namespace vmaf_gui
 {
@@ -16,5 +18,104 @@ namespace vmaf_gui
         {
             InitializeComponent();
         }
+
+        string ChildProcess(string program_name, string args)
+        {
+            // Spawn a child process, record stdout and return it
+            var p = new Process();
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.FileName = program_name;
+            p.StartInfo.Arguments = args;
+
+            p.Start();
+
+            // p.standardoutput is an input stream
+            string output = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+
+            return output;
+        }
+
+        private void btnCompressed_Click(object sender, EventArgs e)
+        {
+            compressedFileDialog.ShowDialog();
+        }
+
+        private void btnSource_Click(object sender, EventArgs e)
+        {
+            originalFileDialog.ShowDialog();
+        }
+
+        private void originalFileDialog_FileOk(object sender, CancelEventArgs e)
+        {
+            string source = originalFileDialog.SafeFileName;
+            lblSource.Text = source;
+        }
+
+        private void compressedFileDialog_FileOk(object sender, CancelEventArgs e)
+        {
+            string compressed = compressedFileDialog.SafeFileName;
+            lblCompressed.Text = compressed;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                originalFileDialog.OpenFile();
+                compressedFileDialog.OpenFile();
+
+                string sourcePath ="\""+ originalFileDialog.FileName + "\"";
+                string compressedPath = "\"" + compressedFileDialog.FileName + "\"";
+
+                Console.WriteLine(sourcePath);
+                Console.WriteLine(compressedPath);
+                try
+                {
+                    if (!Directory.Exists("temp"))
+                    {
+                        Directory.CreateDirectory("temp");
+                    }
+
+                    decompressVideo(sourcePath,"./temp/source.yuv");
+                    decompressVideo(compressedPath,"./temp/compressed.yuv");
+
+                    vmaf();
+                }
+                catch (Exception err)
+                {
+                    MessageBox.Show(err.Message);
+                }
+            }
+            catch
+            {
+                MessageBox.Show("You must select both an original file and a compressed file to compare");
+            }
+        }
+
+        void decompressVideo(string path,string output)
+        {
+            try
+            {
+                ChildProcess("ffmpeg.exe", "-y -i " + path + " -pix_fmt yuv420p -vsync 0 "+ output);
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(path, err.Message);
+            }
+        }
+
+        void vmaf()
+        {
+            ChildProcess("vmaf.exe", "yuv420p 1920 1080 ./temp/source.yuv ./temp/compressed.yuv ./model/vmaf_v0.6.1.pkl --log log.txt");
+        }
     }
+
+   
 }
