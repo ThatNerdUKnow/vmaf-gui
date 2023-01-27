29ad1f2efe4a98fd8ed82059503f9cd1d447e12c
Brandon Piña <brandonpi265@gmail.com>
2023-01-26 18:34:50 -0600

    add tailwind typography

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,10 @@ import { invoke } from "@tauri-apps/api/tauri";
 function App() {
 
   return (
+    <div className="container prose m-5">
+      <h1>Vmaf Gui</h1>
     <button className="btn">Button</button>
+    </div>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,15 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [
+    require("daisyui"),
+    require('@tailwindcss/typography')
+  ],
 }
