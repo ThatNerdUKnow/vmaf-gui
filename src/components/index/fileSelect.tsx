6067bca68fd8a79d9e72dab7f040751c3cbf85cc
Brandon Piña <brandonpi265@gmail.com>
2023-01-28 14:14:49 -0600

    fix path rendering

--- src/components/index/fileSelect.tsx
+++ src/components/index/fileSelect.tsx
@@ -1,16 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { open } from "@tauri-apps/api/dialog";
-import {videoDir as GetVideoDir} from "@tauri-apps/api/path"
+import {
+  videoDir as GetVideoDir,
+  extname,
+  basename,
+} from "@tauri-apps/api/path";
 import Form from "react-bootstrap/Form";
-import { Button, InputGroup, Row } from "react-bootstrap";
+import { Button, InputGroup } from "react-bootstrap";
 
 export type FileSelectProps = {
   type: string;
 };
 
 function FileSelect(props: FileSelectProps) {
-  let [file, setFile] = useState("");
-  let [isInvalid, setIsInvalid] = useState(false);
+  const [file,setFile] = useState("")
+  const [fullPath, setFullPath] = useState("");
+  const [isInvalid, setIsInvalid] = useState(false);
+
+  useEffect(() => {
+    async function calculateFileName() {
+      setFile(await basename(fullPath));
+    }
+    if (fullPath) {
+      calculateFileName();
+    }
+  }, [fullPath]);
 
   async function handleClick(event) {
     event.preventDefault();
@@ -20,22 +34,33 @@ function FileSelect(props: FileSelectProps) {
     const selected = await open({
       multiple: false,
       defaultPath: videoDir,
-      title: `Select ${props.type} video`
+      title: `Select ${props.type} video`,
     });
 
-    if(typeof selected === 'string'){
-      setFile(selected)
+    if (typeof selected === "string") {
+      setFullPath(selected);
     }
   }
 
   return (
-    <Form.Group>
-      <Form.Label htmlFor={props.type}>{props.type}</Form.Label>
-      <InputGroup className="is-invalid" onClick={handleClick}>
-        <Button>Select File:</Button>
-        <Form.Control type="text" value={file} readOnly />
-      </InputGroup>
-    </Form.Group>
+    <>
+      <Form.Group>
+        <Form.Label htmlFor={props.type}>{props.type}</Form.Label>
+        <InputGroup className="is-invalid" onClick={handleClick}>
+          <Button>Select File:</Button>
+          {/* This is a dummy form control that only displays the "file" part of the path */}
+          <Form.Control type="text" value={file} readOnly />
+        </InputGroup>
+      </Form.Group>
+      {/* This is a hidden form control which actually holds the value we want */}
+      <Form.Control
+        name={props.type}
+        type="text"
+        value={fullPath}
+        readOnly
+        style={{ display: "none" }}
+      />
+    </>
   );
 }
 
