7a4d9ac3903ae6c7744b37d825439334dbcddd4b
Brandon Piña <brandonpi265@gmail.com>
2023-01-29 11:55:23 -0600

    bump version of libvmaf-rs

--- src-tauri/src/video.rs
+++ src-tauri/src/video.rs
@@ -4,7 +4,7 @@ use libvmaf_rs::video::Video;
 
 #[tauri::command]
 pub async fn validate_video(path: String) -> Result<(), String> {
-    let did_panic = thread::spawn(|| match Video::new(path, 640, 480) {
+    match Video::new(path, 640, 480) {
         Ok(_) => Ok(()),
         Err(e) => Err(e.frames().into_iter().map(|f| format!("{f:?}")).fold(
             String::new(),
@@ -14,10 +14,5 @@ pub async fn validate_video(path: String) -> Result<(), String> {
                 prev
             },
         )),
-    });
-
-    match did_panic.join() {
-        Ok(e) => e,
-        Err(e) => Err("Panicked while trying to load video".to_string()),
     }
 }
