95ca660ab07ece573327505fb8b8e4fdb50a22cd
Brandon Piña <brandonpi265@gmail.com>
2023-02-15 23:34:41 -0600

    bit depth selection

--- src/components/index/form.tsx
+++ src/components/index/form.tsx
@@ -4,10 +4,18 @@ import { Col, Row } from "react-bootstrap";
 import FileSelect from "./fileSelect";
 import ResolutionSelect from "./resolutionSelect";
 import PixelFormatSelect from "./pixelFormatSelect";
-import { FormEvent, Suspense, useState } from "react";
+import { ChangeEvent, FormEvent, Suspense, useState } from "react";
 import { ReferencePath, DistortedPath } from "../../atoms/videos";
 
 function VmafConfigForm() {
+
+  const [bitDepth,setBitDepth] = useState(8);
+
+  function handleBitDepthChange(event: ChangeEvent<HTMLSelectElement>){
+    let value = parseInt(event.target.value);
+    setBitDepth(value);
+  }
+
   function handleSubmit(event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
   }
@@ -39,7 +47,7 @@ function VmafConfigForm() {
         <Col>
           <Form.Group className="mb-3">
             <Form.Label>Bit Depth</Form.Label>
-            <Form.Select>
+            <Form.Select value={bitDepth} onChange={handleBitDepthChange}>
               <option>8</option>
               <option>10</option>
               <option>12</option>
